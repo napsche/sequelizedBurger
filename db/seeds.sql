761a155136c0cb691ae1625aacff95c6fea03a2f
Nicole Apsche <napsche@kgtiger.com>
2020-07-25 09:31:53 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Bison Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon and Bleu Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", true);
