fafd627a1ae031b4c99a1ee527241515df27e0f8
Nicole Apsche <napsche@kgtiger.com>
2020-07-26 11:57:12 -0400

    updating server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var db = require("./models");
 
 
 // Sets up the Express App
@@ -9,7 +10,7 @@ var methodOverride = require("method-override");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-var db = require("./models");
+
 
 app.use(express.static("./public"));
 app.use(methodOverride("_method"));
